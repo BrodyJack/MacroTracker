59f93f94d5fd8b40096cfed10a770eaa093413eb
Brandon Manuel <brandon-manuel@live.com>
2019-06-07 18:12:39 -0400

    Moved port number into new config file

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
+const config = require('./config/config');
+
 const express = require('express')
 const app = express()
-const port = 3000
+const port = config.port;
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Server started on port ${port}!`))
