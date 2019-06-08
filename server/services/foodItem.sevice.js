86d3d31959f2970096082830edd026728d0477d8
Brandon Manuel <brandon-manuel@live.com>
2019-06-08 00:18:51 -0400

    Changed insertFoodItems to be an actual Promise doing the insert just to make it fake the async insert better

--- server/services/foodItem.sevice.js
+++ server/services/foodItem.sevice.js
@@ -3,10 +3,16 @@ const savedFoodItems = [];
 
 module.exports = {
   insertFoodItems: foodItems => {
-    savedFoodItems.concat(foodItems);
     // return a Promise here just so that the controller
     // is set up to call an async function for when this inevitably
     // does an actual db insert
-    return Promise.resolve('Success!');
+    return new Promise((resolve, reject) => {
+      try {
+        savedFoodItems.concat(foodItems);
+        resolve('Success!');
+      } catch (e) {
+        reject(e);
+      }
+    });
   }
 };
